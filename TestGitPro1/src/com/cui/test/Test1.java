2bed997924258460571fdbd10502404f56295a3a
jyj <jyj@DESKTOP-7NN2IBL>
2018-07-09 15:05:48 +0800

    增加ccccccc输出

--- TestGitPro1/src/com/cui/test/Test1.java
+++ TestGitPro1/src/com/cui/test/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaaaa");
 		System.out.println("bbbbbbbbbb");
+		System.out.println("cccccccccc");
 	}
 
 }
